f6c5414ae4dcb1011a54b7eea1baf96d5f5914bd
Andrii Bilorus <andrii.bilorus@epam.com>
2020-05-05 20:38:51 +0300

    install and configure k8s components

--- ansible/filter_plugins/etcd.py
+++ ansible/filter_plugins/etcd.py
@@ -1,8 +1,15 @@
 
 class FilterModule(object):
   def filters(self):
-    return {'etcd_initial_cluster': self.ip_last_octet}
+    return {
+      'etcd_initial_cluster': self.etcd_init_cluster,
+      'etcd_cluster': self.etcd_cluster
+      }
 
-  def ip_last_octet(self, ip_addrs):
+  def etcd_init_cluster(self, ip_addrs):
     full_addrs = [ f'master-{index}=https://{ip}:2380' for index, ip in enumerate(ip_addrs) ]
+    return ','.join(full_addrs)
+
+  def etcd_cluster(self, ip_addrs):
+    full_addrs = [ f'https://{ip}:2379' for ip in ip_addrs ]
     return ','.join(full_addrs)
